133b6b780ee026f626b231f4f39224d15a8583f8
desssperado <gamzatgasanov0@gmail.com>
2024-10-25 01:42:03 +0300

    Add files via upload

---
+++ moddule_5_4.py
@@ -0,0 +1,31 @@
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super(House, cls).__new__(cls)
+
+        house_name = args[0]
+        cls.houses_history.append(house_name)
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+
+del h2
+del h3
+
+print(House.houses_history)
+del h1
